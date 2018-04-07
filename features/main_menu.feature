8421fcaa1a89cb6cf92489ac569f32c316231069
larate <larisa@portnov.com>
2018-04-07 16:27:21 -0700

    commiting day 1 to the remote repo

---
+++ features/main_menu.feature
@@ -0,0 +1,5 @@
+Feature: Aa a user I want to convert Units and verify
+  Scenario: When i tap on menu item i should see left side window
+    Given I land on Home screen
+    When I press on Menu icon
+    Then I should see left side menu
---
+++ features/step_definitions/main_menu.rb
@@ -0,0 +1,13 @@
+Given("I land on Home screen") do
+  puts "I lans on a home screen"
+end
+
+
+When(/^I press on Menu icon$/) do
+ puts "I press on menu incon"
+end
+
+Then("I should see left side menu") do
+  puts "I should see left side menu"
+end
+
